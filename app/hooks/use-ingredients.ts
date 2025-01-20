21261d16be61470d52eaf2e62f43917dffd88d86
aliskhangazamatov <ag@farcana.com>
2025-01-20 07:04:01 +0300

    add hooks, fix, add interface, refactoring

--- app/hooks/use-ingredients.ts
+++ app/hooks/use-ingredients.ts
@@ -1,18 +1,10 @@
+import { useEffect, useState } from "react";
 import { Ingredient } from "@prisma/client";
 import { Api } from "@/services/api-client";
-import { useEffect, useState } from "react";
-import { useSet } from "react-use";
 
-interface ReturnType {
-  ingredients: Ingredient[];
-  loading: boolean;
-  selectedIngredients: Set<string>;
-  onAddId: (id: string) => void;
-}
-export const useFilterIngredients = (values: string[] = []): ReturnType => {
+export const useIngredients = () => {
   const [ingredients, setIngredients] = useState<Ingredient[]>([]);
   const [loading, setLoading] = useState(false);
-  const [selectedIds, { toggle }] = useSet(new Set<string>(values));
 
   useEffect(() => {
     async function fetchIngredients() {
@@ -31,7 +23,5 @@ export const useFilterIngredients = (values: string[] = []): ReturnType => {
   return {
     ingredients,
     loading,
-    onAddId: toggle,
-    selectedIngredients: selectedIds,
   };
 };
