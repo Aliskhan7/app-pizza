60ad108f9efe5ebec88e2f99bd10bbeeef0a1d3f
aliskhangazamatov <ag@farcana.com>
2025-02-26 07:04:55 +0300

    basket update

--- shared/components/shared/cart-item-details/cart-item-info.tsx
+++ shared/components/shared/cart-item-details/cart-item-info.tsx
@@ -1,39 +1,19 @@
 import { cn } from "@/shared/lib/utils";
-import { mapPizzaType, PizzaSize, PizzaType } from "@/shared/constants/pizza";
-import { Ingredient } from "@prisma/client";
 import { className } from "postcss-selector-parser";
 
 interface Props {
   name: string;
-  pizzaSize?: PizzaSize;
-  type?: PizzaType;
-  ingredients?: Ingredient[];
+  details: string;
 }
 
-export const CartItemInfo: React.FC<Props> = ({
-  name,
-  pizzaSize,
-  type,
-  ingredients,
-}) => {
-  const details = [];
-
-  if (pizzaSize && type) {
-    const typeName = mapPizzaType[type];
-    details.push(`${typeName}, ${pizzaSize} см`);
-  }
-
-  if (ingredients) {
-    details.push(...ingredients.map((ingredient) => ingredient.name));
-  }
-
+export const CartItemInfo: React.FC<Props> = ({ name, details }) => {
   return (
     <div>
       <div className={cn("flex items-center justify-between", className)}>
         <h2 className="text-lg font-bold flex-1 leading-6">{name}</h2>
       </div>
       {details.length > 0 && (
-        <p className="text-xs text-gray-400 w-[90%]">{details.join(", ")}</p>
+        <p className="text-xs text-gray-400 w-[90%]">{details}</p>
       )}
     </div>
   );
