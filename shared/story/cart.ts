738248f7e70a57dde0b0d67605c6543888d10763
aliskhangazamatov <ag@farcana.com>
2025-02-28 17:42:06 +0300

    fix

---
+++ shared/story/cart.ts
@@ -0,0 +1,55 @@
+import { create } from "zustand";
+
+export type ICartItem = {
+  id: number;
+  quantity: number;
+  name: string;
+  imageUrl: string;
+  price: number;
+  pizzaSize?: number | null;
+  type?: number | null;
+  ingredients: Array<{ name: string; price: number }>;
+};
+
+export interface CartState {
+  loading: boolean;
+  error: boolean;
+  totalAmount: number;
+  items: ICartItem[];
+
+  /* Получение товаров из корзины */
+  fetchCartItems: () => Promise<void>;
+
+  /* Запрос на обновление количества товара */
+  updateItemQuantity: (id: number, quantity: number) => Promise<void>;
+
+  /* Запрос на добавление товара в корзину */
+  addCartItem: (values: any) => Promise<void>;
+
+  /* Запрос на удаление товара из корзины */
+  removeCartItem: (id: number) => Promise<void>;
+}
+
+export const useCartStore = create<CartState>((set, get) => ({
+  item: [],
+  error: false,
+  loading: true,
+  totalAmount: 0,
+
+  fetchCartItems: async () => {
+    try {
+      set({ loading: true, error: false });
+      const data = await Api.cart.getCart();
+      set(getCartDetails(data));
+    } catch (error) {
+      console.error(error);
+      set({ error: true });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  updateItemQuantity: async (id: number, quantity: number) => {},
+  addCartItem: async (values: any) => {},
+  removeCartItem: async (id: number) => {},
+}));
