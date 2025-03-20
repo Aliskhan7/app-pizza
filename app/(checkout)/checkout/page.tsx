a1c4ec00ef2ebce93d95a619d7d075030f4f4cf6
AG <aliskhangazamatov@MacBook-Pro-AG.local>
2025-03-20 06:46:33 +0300

    add blocks

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -1,7 +1,38 @@
+import { Container, Title } from "@/shared/components/shared";
 import React from "react";
 
-const Page = () => {
-  return <div></div>;
+export const ChackoutPage = () => {
+  return (
+        <Container className="mt-5">
+          <Title text="Оформление заказа" className="font-extrabold mb-8 text-[36px]" />
+
+
+          
+          <FormProvider {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="flex gap-10">
+                {/* Левая часть */}
+                <div className="flex flex-col gap-10 flex-1 mb-20">
+                  <CheckoutCart
+                    onClickCountButton={onClickCountButton}
+                    removeCartItem={removeCartItem}
+                    items={items}
+                    loading={loading}
+                  />
+
+                  <CheckoutPersonalForm className={loading ? 'opacity-40 pointer-events-none' : ''} />
+
+                  <CheckoutAddressForm className={loading ? 'opacity-40 pointer-events-none' : ''} />
+                </div>
+
+                {/* Правая часть */}
+                <div className="w-[450px]">
+                  <CheckoutSidebar totalAmount={totalAmount} loading={loading || submitting} />
+                </div>
+              </div>
+            </form>
+          </FormProvider>
+        </Container>
+    );
 };
 
-export default Page;
