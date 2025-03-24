982471db72ad4ceed2a49f5ad2c4ee87d3e72cb2
aliskhangazamatov <aliskhanios@gmail.com>
2025-03-24 05:56:41 +0300

    add form hook, make components

---
+++ shared/components/shared/form-components/form-input.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { ErrorText, RequiredSymbol } from "@/shared/components/shared";
+import { Input } from "@/shared/components/ui";
+import { useFormContext } from "react-hook-form";
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label?: string;
+  required?: boolean;
+  className?: string;
+}
+
+export const FormInput: React.FC<Props> = ({
+  className,
+  name,
+  label,
+  required,
+  ...props
+}) => {
+  return (
+    <div className={className}>
+      {label && (
+        <p className="font-medium mb-2">
+          {label} {required && <RequiredSymbol />}
+        </p>
+      )}
+
+      <div className="relative">
+        <Input className="h-12 text-md" {...props} />
+      </div>
+
+      <ErrorText text={errorText} className="mt-2" />
+    </div>
+  );
+};
+
+export default FormInput;
