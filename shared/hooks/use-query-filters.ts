102bebc8abfcc9a7883185f6cb19ddb716e75acf
aliskhangazamatov <aliskhanios@gmail.com>
2025-04-04 17:03:10 +0300

    fix

--- shared/hooks/use-query-filters.ts
+++ shared/hooks/use-query-filters.ts
@@ -1,29 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import qs from "qs";
 import { useRouter } from "next/navigation";
 import { Filters } from "@/shared/hooks/use-filters";
 
 export const useQueryFilters = (filters: Filters) => {
   const router = useRouter();
-  console.log("Current filters:", filters);
+  const isMounted = useRef(false);
+
   useEffect(() => {
-    const params = {
-      ...filters.prices,
-      pizzaTypes: Array.from(filters.pizzaTypes),
-      sizes: Array.from(filters.sizes),
-      ingredients: Array.from(filters.selectedIngredients),
-    };
+    if (isMounted) {
+      const params = {
+        ...filters.prices,
+        pizzaTypes: Array.from(filters.pizzaTypes),
+        sizes: Array.from(filters.sizes),
+        ingredients: Array.from(filters.selectedIngredients),
+      };
 
-    const query = qs.stringify(params, {
-      arrayFormat: "comma",
-    });
-    if (window.location.search === `?${query}`) {
-      return; // ⛔ Если URL уже такой же, выходим из useEffect
+      const query = qs.stringify(params, {
+        arrayFormat: "comma",
+      });
+      if (window.location.search === `?${query}`) {
+        return; // ⛔ Если URL уже такой же, выходим из useEffect
+      }
+
+      router.push(`?${query}`, {
+        scroll: false,
+      });
     }
-    console.log("Updating query string:", query);
 
-    router.push(`?${query}`, {
-      scroll: false,
-    });
+    isMounted.current = true;
   }, [filters]);
 };
