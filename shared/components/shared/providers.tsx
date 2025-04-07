4228c060e3000221ed93d751fe8e45277aec4db0
aliskhangazamatov <aliskhanios@gmail.com>
2025-04-07 10:13:24 +0300

    add loader, create provider

---
+++ shared/components/shared/providers.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
+import NextTopLoader from "nextjs-toploader";
+
+export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      <SessionProvider>{children}</SessionProvider>
+      <Toaster />
+      <NextTopLoader />
+    </>
+  );
+};
